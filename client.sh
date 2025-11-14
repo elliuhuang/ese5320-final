3fde9afd77cb756f9583dce7f65ec72932869a06
21bsirota <sirotab@seas.upenn.edu>
2025-11-14 13:56:23 -0500

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-./client -f LittlePrince.txt -i 10.10.7.1 -s 50000
+input_file="LittlePrince.txt"
+inflag=0
+
+for arg in $@; do
+    if [ "$arg" == "-f" ]; then
+	    inflag=1
+    elif [ $inflag == 1 ]; then
+        input_file=$arg
+        inflag=0
+    else
+        echo "Warning: "${arg}" is not a recognized command option"
+        echo ""
+    fi
+done
+
+./client -f ${input_file} -i 10.10.7.1 -s 50000
